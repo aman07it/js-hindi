6a6de826f76e155b41bda7d4d393ecf5a5e077a1
Aman Raj <118063706+aman07it@users.noreply.github.com>
2024-06-16 18:32:42 +0000

    data

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+
+ const accountId = 144553
+ let accountEmail = "hitesh@google.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+ // accountId = 2 // not allowed 
+
+ accountEmail = "hc@hc.com"
+ accountPassword="21212121"
+ accountCity="Bihar"
+
+ console.log(accountId);
+ /*
+ mostly let and const used .
+ if we not assign the let varible to anyrhi g then thye show undefined.
+ */
+ console.table([accountEmail,accountCity,accountPassword])
