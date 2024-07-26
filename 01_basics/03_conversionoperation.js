1fcbad899399c2b98d992a7210ef58c5712a7727
Aman Raj <118063706+aman07it@users.noreply.github.com>
2024-07-26 17:49:21 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,6 +1,6 @@
 let score = "hitesh"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 //console.log(typeof value InNumber);
@@ -11,7 +11,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn=1 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // IIII => false
@@ -19,5 +19,41 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//operation 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+//console.log(2-2);
+//console.log(2+2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = strl + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(+true);
+console.log(+""); I
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let game = 100
+game++;
+console.log(game); 
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.
+ 
+
+
